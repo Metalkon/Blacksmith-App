b34534265862ef3abf3203da9262bae86e2826f6
Metalkon <MetalkonSC@hotmail.com>
2024-02-23 08:23:39 -0400

    Blazored Localstorage & CustomAuthStateProvider (from older project)

---
+++ Blacksmith.Blazor/Services/CustomAuthStateProvider.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text.Json;
+using Blazored.LocalStorage;
+
+namespace Blacksmith.Blazor.Services
+
+{
+    public class CustomAuthStateProvider : AuthenticationStateProvider
+    {
+        private readonly ILocalStorageService _localStorage;
+        private readonly HttpClient _http;
+
+        public CustomAuthStateProvider(ILocalStorageService localStorage, HttpClient http)
+        {
+            _localStorage = localStorage;
+            _http = http;
+        }
+
+        // Retrieves the authentication state asynchronously.
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            string token = await _localStorage.GetItemAsStringAsync("token");
+
+            var identity = new ClaimsIdentity();
+            _http.DefaultRequestHeaders.Authorization = null;
+
+            if (!string.IsNullOrEmpty(token))
+            {
+                // Parse the claims from the JWT token and create a ClaimsIdentity.
+                identity = new ClaimsIdentity(ParseClaimsFromJwt(token), "jwt");
+                // Set the Authorization header with the token.
+                _http.DefaultRequestHeaders.Authorization =
+                    new AuthenticationHeaderValue("Bearer", token.Replace("\"", ""));
+            }
+
+            var user = new ClaimsPrincipal(identity);
+            var state = new AuthenticationState(user);
+
+            NotifyAuthenticationStateChanged(Task.FromResult(state));
+
+            return state;
+        }
+
+        // Parses the claims from the JWT token.
+        public static IEnumerable<Claim> ParseClaimsFromJwt(string jwt)
+        {
+            var payload = jwt.Split('.')[1];
+            var jsonBytes = ParseBase64WithoutPadding(payload);
+            var keyValuePairs = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonBytes);
+            return keyValuePairs.Select(kvp => new Claim(kvp.Key, kvp.Value.ToString()));
+        }
+
+        // Parses a base64 string without padding to a byte array.
+        private static byte[] ParseBase64WithoutPadding(string base64)
+        {
+            switch (base64.Length % 4)
+            {
+                case 2:
+                    base64 += "==";
+                    break;
+                case 3:
+                    base64 += "=";
+                    break;
+            }
+            return Convert.FromBase64String(base64);
+        }
+    }
+}
